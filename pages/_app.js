c4c426afbbae9166ae5cd61dae230ece6d30ca38
Yen <YenXXXW@gmail.com>
2022-10-10 11:33:44 +0630

    added Head section

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
 import '../styles/globals.css'
 import Navbar from '../components/Navbar'
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return(
    <>
+  <Head>
+    <title>Wai's Portfolio</title>
+    <meta name='description' content='portfolio website created by Wai'/>
+    <link rel='icon' href='/w.jpg'></link>
+  </Head> 
   <Navbar />
   <Component {...pageProps} />
   </> 
