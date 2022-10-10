fcf3093e86cdfb720f759e951750c057ee67b501
Yen <YenXXXW@gmail.com>
2022-10-10 11:52:34 +0630

    logo not appearing error

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,17 @@ import About from '../components/about'
 import Skills from '../components/skills'
 import Projects from '../components/projects'
 import Contact from '../components/contact'
+import Head from 'next/head'
 
 export default function Home() {
   
   return (
     <div>
+      <Head>
+      <title>Wai&apos;s Portfolio</title>
+      <meta name='description' content='portfolio website created by Wai'/>
+      <link rel='icon' href='/w.jpg'></link>
+    </Head>
       <Main />
       <About />
       <Skills />
